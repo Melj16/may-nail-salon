3a190b4bfb8bfcb3434316ff3d9e136bda9df4bc
Melissa Jiang <melissajiang3@gmail.com>
2026-02-08 23:04:18 -0500

    test

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -13,7 +13,7 @@ export function ContactSection() {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("/", {
+      const response = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData as any).toString(),
